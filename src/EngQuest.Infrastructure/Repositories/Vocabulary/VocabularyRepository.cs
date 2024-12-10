37f6763613598af795f5fb752d7a1255121e2e63
Stas <stasnocap@gmail.com>
2024-12-10 20:33:46 +0300

    working

--- src/EngQuest.Infrastructure/Repositories/Vocabulary/VocabularyRepository.cs
+++ src/EngQuest.Infrastructure/Repositories/Vocabulary/VocabularyRepository.cs
@@ -14,11 +14,8 @@ public class VocabularyRepository(
     ModalVerbRepository _modalVerbRepository,
     NounRepository _nounRepository,
     PrimaryVerbRepository _primaryVerbRepository,
-    AdverbRepository _adverbRepository,
-    CompoundRepository _compoundRepository,
     VerbRepository _verbRepository,
-    NumberWithNounRepository _numberWithNounRepository,
-    PronounRepository _pronounRepository) : IVocabularyRepository
+    NumberWithNounRepository _numberWithNounRepository) : IVocabularyRepository
 {
     public async Task<List<string>> GetRandomAsync(Word word, int count, IDbConnection dbConnection, CancellationToken cancellationToken)
     {
@@ -30,24 +27,49 @@ public class VocabularyRepository(
             case WordType.Language:
             case WordType.Preposition:
             case WordType.QuestionWord:
-                string wordText = word.Text.GetWord();
-                
-                string tableName = TableNames.GetTableName(word.Type);
-                
-                string sql = $"""
-                              SELECT text FROM {tableName}
-                              WHERE text != @Word
-                              ORDER BY random()
-                              LIMIT @Count
-                              """;
-                
-                IEnumerable<string> words = await dbConnection.QueryAsync<string>(sql, new { Word = wordText, Count = count });
+                {
+                    string wordText = word.Text.GetWord();
 
-                return words.ToList();
+                    string tableName = TableNames.GetTableName(word.Type);
+
+                    string sql = $"""
+                                  SELECT text FROM {tableName}
+                                  WHERE text != @Word
+                                  ORDER BY random()
+                                  LIMIT @Count
+                                  """;
+
+                    IEnumerable<string> words = await dbConnection.QueryAsync<string>(sql, new { Word = wordText, Count = count });
+
+                    return words.ToList();
+                }
             case WordType.Adverb:
-                return await _adverbRepository.GetRandomAdverbsAsync(word, count, cancellationToken);
             case WordType.Compound:
-                return await _compoundRepository.GetRandomCompoundsAsync(word, count, cancellationToken);
+            case WordType.Pronoun:
+                {
+                    string wordText = word.Text.GetWord().Value;
+
+                    string tableName = TableNames.GetTableName(word.Type);
+
+                    string sql = $"""
+                                  WITH cte AS (SELECT id, type
+                                               FROM {tableName}
+                                               WHERE text = @Text
+                                               LIMIT 1)
+                                               
+                                  SELECT text
+                                  FROM {tableName} AS x
+                                  WHERE (SELECT COUNT(*) FROM cte) = 0
+                                     OR (x.type = (SELECT type FROM cte) AND x.id != (SELECT id FROM cte))
+                                  ORDER BY random()
+                                  LIMIT @Count;
+                                  """;
+
+
+                    IEnumerable<string> words = await dbConnection.QueryAsync<string>(sql, new { Count = count, Text = wordText });
+
+                    return words.ToList();
+                }
             case WordType.ComparisonAdjective:
                 return await comparisonAdjectiveRepository.GetRandomComparisonAdjectivesAsync(word, count, cancellationToken);
             case WordType.LetterNumber:
@@ -62,8 +84,6 @@ public class VocabularyRepository(
                 return await _verbRepository.GetRandomVerbsAsync(word, count, cancellationToken);
             case WordType.NumberWithNoun:
                 return await _numberWithNounRepository.GetRandomNumberWithNounsAsync(word, count, cancellationToken);
-            case WordType.Pronoun:
-                return await _pronounRepository.GetRandomPronounsAsync(word, count, cancellationToken);
             case WordType.None:
             default:
                 throw new ApplicationException();
